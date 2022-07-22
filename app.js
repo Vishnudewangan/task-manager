b0dba624f5ef89676735c4974244a57fde5a47b0
Vishnudewangan <dewanganvish1721@gmail.com>
2022-07-23 01:08:12 +0530

    set it up upto express server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App');
+});
+
+
+
+
+const port= 3000;
+
+app.listen(port, console.log(`Server is listining on port ${port}...`))
